ed7f604345892d5ff0dc75e551f47d17b95ccbe4
Cavanhaman <cavanhaman@hotmail.com>
2018-02-06 22:15:27 -0200

    Primeiros exercicios POO

---
+++ src/ExercicioPOO1.java
@@ -0,0 +1,20 @@
+/*
++--------------------+
+| Rodrigo CavanhaMan |
+|  Exercícios POO 1  |
++--------------------+
+*/
+import java.util.Scanner;
+
+public class ExercicioPOO1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String X = "Hello World!";
+		System.out.println(X);
+
+		sc.close();
+	}
+}
+
---
+++ src/projeto1/Exercicios.java
@@ -0,0 +1,20 @@
+package projeto1;
+/*
++---------------------+
+| Rodrigo CavanhaMan  |
+|   Exercícios POO    |
++---------------------+
+*/
+import java.util.Scanner;
+
+public class Exercicios {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		System.out.println(X);
+
+		sc.close();
+	}	
+}
