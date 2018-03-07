d34d60657ee93c53a6bcf3854b110de85985faf5
CavanhaMan <cavanhaman@yahoo.com.br>
2018-03-06 22:14:43 -0300

    Add files via upload

---
+++ lista7_06_03/SimpleGetterAndSetter.java
@@ -0,0 +1,13 @@
+package lista7_06_03;
+
+public class SimpleGetterAndSetter {
+    private int number;
+ 
+    public int getNumber() {
+        return this.number;
+    }
+ 
+    public void setNumber(int num) {
+        this.number = num;
+    }
+}
