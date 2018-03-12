7248067a391dba9d3630767a6ff07d077408f149
CavanhaMan <cavanhaman@yahoo.com.br>
2018-03-12 11:28:24 -0300

    Add files via upload

--- lista7_06_03/Ex5/Programa.java
+++ lista7_06_03/Ex5/Programa.java
@@ -8,13 +8,12 @@ public class Programa {
 	public static void main(String[] args) {
 		List<Funcionario> funcionarios = new ArrayList<Funcionario>();
 		
-		funcionarios.add(new Assistente("b","10",100.0));
-		funcionarios.add(new Vendedor("c","10",100.0, 20.0));
-		funcionarios.add(new Gerente("a","10",100.0));
+		funcionarios.add(new Assistente("Assistente b","10",100.0));
+		funcionarios.add(new Vendedor("Vendedor c","10",100.0, 20.0));
+		funcionarios.add(new Gerente("Gerente a","10",100.0));
 		
 		for (Funcionario funcionario : funcionarios) {
-			//System.out.println("Pagar para o funcionário " + funcionario.getNome() + " -> " + funcionario.calculaSalario());
-			System.out.println("Pagar para o funcionário " + funcionario.getNome() + " -> " + funcionario.calculaSalario());
+			System.out.println("Pagar para o funcionário " + funcionario.getNome() + " -> R$" + funcionario.calculaSalario());
 		}
 		
 	}
