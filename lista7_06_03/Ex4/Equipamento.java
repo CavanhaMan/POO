885b0cb5144cca6fa41ba2c101d4e751ff74bbbf
CavanhaMan <cavanhaman@yahoo.com.br>
2018-03-12 17:41:11 -0300

    Add files via upload

---
+++ lista7_06_03/Ex4/Equipamento.java
@@ -0,0 +1,22 @@
+package lista7_06_03.Ex4;
+
+public class Equipamento {
+	private boolean ligado;
+
+	public Equipamento() { super(); }
+
+	public Equipamento(boolean ligado) {
+		super();
+		this.ligado = ligado;
+	}
+
+	public boolean isLigado() { return ligado; }
+	public void setLigado(boolean ligado) { this.ligado = ligado; }
+	
+	void liga() {
+		this.ligado=true;
+	}
+	void desliga() {
+		this.ligado=false;
+	}
+}
---
+++ lista7_06_03/Ex4/EquipamentoSonoro.java
@@ -0,0 +1,30 @@
+package lista7_06_03.Ex4;
+
+public class EquipamentoSonoro extends Equipamento {
+	private int volume;
+	private boolean stereo;
+
+	public EquipamentoSonoro() { super(); }
+	public EquipamentoSonoro(boolean ligado) { super(ligado); }
+	public int getVolume() { return volume; }
+	public void setVolume(int volume) { this.volume = volume; }
+	public boolean isStereo() { return stereo; }
+	public void setStereo(boolean stereo) { this.stereo = stereo; }
+	public EquipamentoSonoro(int volume, boolean stereo) {
+		super();
+		this.volume = volume;
+		this.stereo = stereo;
+	}
+	
+	void mono() {
+		//Torna o atributo stereo falso
+	}
+	void stereo() {
+		//Torna o atributo stereo verdadeiro
+	}
+	void liga() {
+		//Efeito Sobrescreve o método liga da superclasse.
+		//Este método deve chamar o método liga da superclasse e ajustar o atributo volume para 5
+	}
+	
+}
